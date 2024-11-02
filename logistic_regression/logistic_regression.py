b932887be0284e55f3d73e62ec3726bd111c7ab3
mohan_g <mohanreddy.go@gmail.com>
2024-11-03 00:44:04 +0530

    logistic regression

---
+++ logistic_regression/logistic_regression.py
@@ -0,0 +1,37 @@
+
+import numpy as np 
+
+def sigmoid(z): 
+    return 1/(1+np.exp(-z))
+
+class LogisticRegression: 
+    def __init__(self, lr=0.001, n_iters=1000, weights=None, bias=None): 
+        self.lr = lr 
+        self.n_iters = n_iters 
+        self.weights = weights 
+        self.bias = bias 
+    
+    def fit(self, X, y):  # X_train, y_train
+        n_samples, n_features = X.shape 
+
+        # Initiaze the weights and bias 
+        self.weights = np.zeros(n_features)
+        self.bias = 0 
+
+        # Generate preds 
+        for iter in range(self.n_iters): 
+            z = np.dot(X, self.weights) + self.bias 
+            y_preds = sigmoid(z)
+
+            dw = (1/n_samples)*(np.dot(X.T, (y_preds - y)))
+            db = (1/n_samples)*(np.sum(y_preds - y))
+
+            self.weights = self.weights - (self.lr*dw)
+            self.bias = self.bias - (self.lr*db)
+    
+    def predict(self, X): # X_test 
+        z = np.dot(X, self.weights) + self.bias 
+        y_preds = sigmoid(z)
+        return y_preds
+
+        
